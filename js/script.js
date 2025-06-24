ad70d7ec938f788f28ce1d488f3b8dc8a409fd03
Jashimjnn <mdjashimuddinrubel8305@gmail.com>
2025-06-24 23:04:28 +0600

    portf commit

--- js/script.js
+++ js/script.js
@@ -55,3 +55,28 @@ const workItems = document.querySelectorAll('.work-img');
 workItems.forEach(item => {
   observ.observe(item);
 });
+
+
+
+
+
+
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        const increment = target / 100;
+
+        if (count < target) {
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(updateCount, 30);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCount();
+});
