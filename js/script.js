cc6b87a55bcfaba31ef1a39a8ac4385952726d89
Jashimjnn <mdjashimuddinrubel8305@gmail.com>
2025-06-24 14:23:02 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const navbar = document.getElementById('nav-link')
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+
+if (bar) {
+  bar.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+    console.log(true);
+  });
+}
+
+if (close) {
+  close.addEventListener('click', () => {
+    navbar.classList.remove('active');
+  });
+}
+
+
+
+
+
+//Bennar Animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.5,
+});
+
+document.querySelectorAll('.animate-on-scroll').forEach(el => {
+  observer.observe(el);
+});
+
+
+const observ = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+const workItems = document.querySelectorAll('.work-img');
+workItems.forEach(item => {
+  observ.observe(item);
+});
